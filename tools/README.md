cb5889a8796d823c9d5b0ae0c33e0f6be4cf4c40
Franziska Krüger <kruegerfr@gmail.com>
2016-05-25 16:37:12 +0200

    Adds deconv

--- tools/README.md
+++ tools/README.md
@@ -1,3 +1,6 @@
 # Tools
 
 Some small programs that make it possible to use just some code snippets from the original project, like the convolution method.
+
+**conv2** - convolves an image with a kernel
+**deconv** - deconvolves an image with a kernel
---
+++ tools/deconvolve.cpp
@@ -0,0 +1,64 @@
+/***********************************************************************
+ * Author:       Franziska Krüger
+ *
+ * Description:
+ * ------------
+ * Deconvolves an image with a kernel using the deconvolveFFT and the
+ * deconvolveIRLS method.
+ * 
+ ************************************************************************
+*/
+
+#include <iostream>                     // cout, cerr, endl
+#include <stdexcept>                    // throw exception
+
+#include <opencv2/highgui/highgui.hpp>  // imread, imshow, imwrite
+#include <opencv2/imgproc/imgproc.hpp>  // convert
+
+#include "deconvolution.hpp"
+
+using namespace std;
+using namespace cv;
+
+
+int main(int argc, char** argv) {
+    Mat src, kernel, mask, deconv;
+
+    if (argc < 3) {
+        cerr << "usage: deconv <image> <kernel> [<mask>]" << endl;
+        return 1;
+    }
+
+    string image = argv[1];
+    string kernelName = argv[2];
+
+    src = imread(image, CV_LOAD_IMAGE_GRAYSCALE);
+    src.convertTo(src, CV_32F);
+    src /= 255;
+
+    kernel = imread(kernelName, CV_LOAD_IMAGE_GRAYSCALE);
+
+    if (!src.data || !kernel.data) {
+        throw runtime_error("Can not load images!");
+    }
+
+    kernel.convertTo(kernel, CV_32F);
+    kernel /= sum(kernel)[0];  // kernel is not energy preserving
+
+    // load mask
+    if (argc > 3) {
+        string maskName = argv[3];
+        mask = imread(maskName, CV_LOAD_IMAGE_GRAYSCALE);
+    } else {
+        // mask for whole image
+        mask = Mat::ones(src.size(), CV_32F);
+    }
+
+    deblur::deconvolveFFT(src, deconv, kernel);
+    imwrite("deconvFFT.png", deconv);
+
+    deblur::deconvolveIRLS(src, deconv, kernel, mask);
+    imwrite("deconvIRLS.png", deconv);
+
+    return 0;
+}
